bd9b97dfc35f81888ef142978325cb3272298942
Riya8216 <kasariya24@gmail.com>
2026-03-12 00:18:49 +0530

    Add minimumSize method to Solution class
    Implement binary search to find minimum size with max operations.

--- ques2/ques2.py
+++ ques2/ques2.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def minimumSize(self, nums, maxOperations):
+        lowest=1
+        highest=max(nums)
+        penalty=highest
+        while lowest<=highest:
+            mid=lowest+(highest-lowest)//2
+            operations=0
+            for x in nums:
+                operations+=(x-1)//mid
+            if operations<=maxOperations:
+                penalty=mid
+                highest=mid-1
+            else:
+                lowest=mid+1
+        return penalty                
+          
+        
+        
 
